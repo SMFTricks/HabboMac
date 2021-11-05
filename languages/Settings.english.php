c33b2934eaf685622c5e8ac2b469bc7041f95b28
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-04 18:06:48 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0 RC5; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Habbo Mac.<br /><br />Author: <a target="_blank" href="https://smftricks.com">SMF Tricks Team</a>';
+
+?>
